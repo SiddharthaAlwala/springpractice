263c21ec667a4f74da69be1c7e6f557ee3ecfafe
siddhartha <siddhartha1197@gmail.com>
2024-08-06 10:57:10 -0500

    "dependecy injection through spring boot"

--- src/main/java/com/springframework/myApp/MyAppApplication.java
+++ src/main/java/com/springframework/myApp/MyAppApplication.java
@@ -2,12 +2,15 @@ package com.springframework.myApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MyAppApplication.class, args);
+		Car obj = context.getBean(Car.class);
+		obj.move();
 	}
 
 }
